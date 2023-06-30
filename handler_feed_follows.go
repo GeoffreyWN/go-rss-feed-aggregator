61738b0880bc3138edf3b1350984b475d97010d7
weshsnr <ndungugeoffrey11@gmail.com>
2023-06-30 16:58:35 +0300

    chore: delete a feed follow

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -7,6 +7,7 @@ import (
 	"rss-aggregator/internal/database"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 )
 
@@ -53,4 +54,27 @@ func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Re
 	}
 
 	respondWithJSON(w, 201, databaseFeedFollowsToFeedFollows(feedFollow))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+	feedFollowID, err := uuid.Parse(feedFollowIDStr)
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing feed follow id: %v", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
+		ID:     feedFollowID,
+		UserID: user.ID,
+	})
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error deleting feed follow: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, struct{}{})
+
 }
